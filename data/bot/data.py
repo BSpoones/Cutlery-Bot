4f2e495b8a0060d834fe6b429d5061c47e4e5a0d
Bspoones <ben.soones@gmail.com>
2021-11-13 09:41:22 +0000

    init

---
+++ data/bot/data.py
@@ -0,0 +1,9 @@
+import json
+with open("./data/bot/BotInfo.json") as f:
+    BOT_INFO = json.load(f)
+    VERSION = BOT_INFO["version"]
+    ACTIVITY_TYPE = BOT_INFO["activity type"]
+    ACTIVITY_NAME = BOT_INFO["activity name"]
+    TRUSTED_IDS = BOT_INFO["trusted ids"]
+    OWNER_IDS = BOT_INFO["owner ids"]
+        
