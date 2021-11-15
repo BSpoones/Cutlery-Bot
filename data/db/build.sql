360347bde2e981d0a2a64f4f53864ebe1d41346e
Bspoones <ben.soones@gmail.com>
2021-11-15 20:20:00 +0000

    Changed data types

--- data/db/build.sql
+++ data/db/build.sql
@@ -1,51 +1,51 @@
 CREATE TABLE IF NOT EXISTS Lessons (
-    LessonID int PRIMARY KEY,
-    GroupID int,
-    TeacherID int,
+    LessonID INT(32) PRIMARY KEY,
+    GroupID INT(32),
+    TeacherID INT(32),
     DayOfWeek tinyint(6),
-    StartTime time,
-    EndTime time,
-    Room char
+    StartTime TIME,
+    EndTime TIME,
+    Room VARCHAR(50)
 );
 
 CREATE TABLE IF NOT EXISTS Teachers (
-    TeacherID int PRIMARY KEY,
-    GroupID int,
-    TeacherName char,
-    TeacherSubject text,
-    TeacherColour char,
-    TeacherLink text,
-    LessonOnline tinyint(1)
+    TeacherID INT(32) PRIMARY KEY,
+    GroupID INT(32),
+    TeacherName VARCHAR(50),
+    TeacherSubject VARCHAR(100),
+    TeacherColour VARCHAR(7),
+    TeacherLink VARCHAR(256),
+    LessonOnline BOOL
 );
 
-CREATE TABLE IF NOT EXISTS Groups (
-    GroupID int PRIMARY KEY,
-    GroupOwnerID char(18),
-    GuildID char(18),
-    GroupCode char,
-    GroupName text,
-    RoleID char(18),
-    RoleColour char,
-    CaregoryID char(18),
-    LessonAnnouncementID char(18),
-    NLDayID char(18),
-    NLTimeID char(18),
-    ImageLink text,
-    AlertTimes text
+CREATE TABLE IF NOT EXISTS LessonGroups (
+    GroupID INT(32) PRIMARY KEY,
+    GroupOwnerID VARCHAR(18),
+    GuildID VARCHAR(18),
+    GroupCode VARCHAR(50),
+    GroupName VARCHAR(50),
+    RoleID VARCHAR(18),
+    RoleColour VARCHAR(7),
+    CaregoryID VARCHAR(18),
+    LessonAnnouncementID VARCHAR(18),
+    NLDayID VARCHAR(18),
+    NLTimeID VARCHAR(18),
+    ImageLink VARCHAR(256),
+    AlertTimes VARCHAR(100)
 );
 
 CREATE TABLE IF NOT EXISTS Students (
     StudentID int PRIMARY KEY,
     GroupID int,
-    UserID char(18),
+    UserID VARCHAR(18),
     Fullname text
 );
 
 CREATE TABLE IF NOT EXISTS Assignments(
     AssignmentID int PRIMARY KEY,
-    CreatorUserID char(18),
-    GroupID char(18),
-    TeacherID char(18),
+    CreatorUserID VARCHAR(18),
+    GroupID VARCHAR(18),
+    TeacherID VARCHAR(18),
     DueDatetime datetime,
     AssignmentContent text,
     TimeSent timestamp DEFAULT CURRENT_TIMESTAMP
@@ -53,10 +53,10 @@ CREATE TABLE IF NOT EXISTS Assignments(
 
 CREATE TABLE IF NOT EXISTS DateReminders (
     ReminderID int PRIMARY KEY,
-    CreatorUserID char(18),
-    TargetID char(18),
-    OutputGuildID char(18),
-    OutputChannelID char(18),
+    CreatorUserID VARCHAR(18),
+    TargetID VARCHAR(18),
+    OutputGuildID VARCHAR(18),
+    OutputChannelID VARCHAR(18),
     ReminderDatetime datetime,
     Content text,
     TimeSent timestamp DEFAULT CURRENT_TIMESTAMP
@@ -64,10 +64,10 @@ CREATE TABLE IF NOT EXISTS DateReminders (
 
 CREATE TABLE IF NOT EXISTS RepeatingReminders (
     ReminderID int PRIMARY KEY,
-    CreatorUserID char(18),
-    TargetID char(18),
-    OutputGuildID char(18),
-    OutputChannelID char(18),
+    CreatorUserID VARCHAR(18),
+    TargetID VARCHAR(18),
+    OutputGuildID VARCHAR(18),
+    OutputChannelID VARCHAR(18),
     RepeatType char,
     ReminderDay char,
     Content text,
@@ -75,18 +75,19 @@ CREATE TABLE IF NOT EXISTS RepeatingReminders (
 );
 
 CREATE TABLE IF NOT EXISTS CommandLogs(
-    UserID char(18),
-    GuildID char(18),
-    ChannelID char(18),
+    UserID VARCHAR(18),
+    GuildID VARCHAR(18),
+    ChannelID VARCHAR(18),
     Command text,
+    Args text DEFAULT NULL,
     TimeSent timestamp DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE IF NOT EXISTS MessageLogs(
-    UserID char(18),
-    GuildID char(18),
-    ChannelID char(18),
-    MessageID char(18),
+    UserID VARCHAR(18),
+    GuildID VARCHAR(18),
+    ChannelID VARCHAR(18),
+    MessageID VARCHAR(18),
     Command text,
     TimeSent timestamp DEFAULT CURRENT_TIMESTAMP
-);
+)
