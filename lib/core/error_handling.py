aee3b2b496f84f9b4cb8857ca216d757f97caad2
Bspoones <ben.soones@gmail.com>
2021-12-17 18:48:10 +0000

    Fixed issue where exception args would not send

--- lib/core/error_handling.py
+++ lib/core/error_handling.py
@@ -5,7 +5,7 @@ HOOKS = tanjun.AnyHooks()
 @HOOKS.with_on_error
 async def on_error(ctx: tanjun.abc.Context, exc: Exception):
     exception_type = (type(exc).__name__)
-    exception_args = "\n".join(exc.args)
+    exception_args = "\n".join(str(exc.args))
     # Has to be imported in func as Bot class uses this to init
     from lib.core.bot import Bot 
     embed = Bot.auto_embed(
@@ -15,5 +15,5 @@ async def on_error(ctx: tanjun.abc.Context, exc: Exception):
         description=f"{exception_args}",
         ctx=ctx
     )
-    logging.error(f"{exception_type} {exception_args}")
+    logging.error(f"{exception_type} {str(exception_args)}")
     await ctx.respond(embed=embed)
