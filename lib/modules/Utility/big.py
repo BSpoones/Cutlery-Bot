434befeb021ee7b44ebbfaa3703f1159c17d7148
Bspoones <ben.soones@gmail.com>
2021-12-18 15:20:34 +0000

    Added error checking and a custom emoji timestamp

--- lib/modules/Utility/big.py
+++ lib/modules/Utility/big.py
@@ -3,6 +3,7 @@ from lib.core.bot import Bot
 from lib.core.client import Client
 import datetime as dt
 from . import COG_LINK, COG_TYPE
+import requests,logging
 
 from tanjun.abc import Context as Context
 
@@ -14,22 +15,35 @@ big_component = tanjun.Component()
 async def big_command(ctx: Context, emoji: hikari.CustomEmoji):
     try:
         emoji = hikari.CustomEmoji.parse(emoji)
+        created_at = int(emoji.created_at.timestamp())
+        description = f"> Created on <t:{created_at}:d>"
     except:
         emoji = hikari.UnicodeEmoji.parse(emoji)
-    print(str(emoji.url))
-    embed = Bot.auto_embed(
-        type="emoji",
-        author = COG_TYPE,
-        author_url = COG_LINK,
-        title =f"Showing an enlarged `{emoji.name}`",
-        emoji_url= emoji.url,
-        ctx=ctx
-        )
-    await ctx.respond(embed=embed)
-    Bot.log_command(ctx,"big",str(emoji.name))
-
-
-
+        description = None
+    try: # In a try except as it requires a website to work
+        f = requests.get(emoji.url)
+        if f.text == "404: Not Found": # Checks if input is an emoji or not
+            good_emoji = False
+        else:
+            good_emoji = True
+    except:
+        good_emoji = True
+        logging.error("EMOJI CHECKING WEBSITE DOWN")
+    if good_emoji:
+        embed = Bot.auto_embed(
+            type="emoji",
+            author = COG_TYPE,
+            author_url = COG_LINK,
+            title =f"Showing an enlarged `{emoji.name}`",
+            description = description,
+            emoji_url= emoji.url,
+            ctx=ctx
+            )
+        await ctx.respond(embed=embed)
+        Bot.log_command(ctx,"big",str(emoji.name))
+    else:
+        raise ValueError("Please enter a valid emoji")
+    
 @tanjun.as_loader   
 def load_components(client: Client):
     client.add_component(big_component.copy())
