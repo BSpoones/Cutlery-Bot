8158c6c26c1060bcef818ddea46ac6bb3e316713
Bspoones <ben.soones@gmail.com>
2021-12-18 16:44:21 +0000

    Cleaned up code

--- lib/modules/Utility/ping.py
+++ lib/modules/Utility/ping.py
@@ -1,10 +1,15 @@
-import hikari, tanjun
+"""
+/ping command
+Developed by Bspoones - Dec 2021
+Solely for use in the ERL discord bot
+Doccumentation: https://www.bspoones.com/ERL/Utility#Ping
+"""
+
+import tanjun
+from tanjun.abc import Context as Context
 from lib.core.bot import Bot
 from lib.core.client import Client
-import datetime as dt
-from . import COG_TYPE
-
-from tanjun.abc import Context as Context
+from . import COG_TYPE, COG_LINK
 
 ping_component = tanjun.Component()
 
@@ -15,14 +20,13 @@ async def ping_command(ctx: Context):
     embed = Bot.auto_embed(
         type="info",
         author=f"{COG_TYPE}",
-        title="Ping",
-        description=f"> ERL bot ping: `{ping:,.0f}` ms.",
+        author_url = COG_LINK,
+        title=":stopwatch: Ping",
+        description=f"> ERL ping: `{ping:,.0f}` ms.",
         ctx=ctx
     )
     await ctx.respond(embed)
     Bot.log_command(ctx,"ping")
-
-
 
 @tanjun.as_loader
 def load_components(client: Client):
