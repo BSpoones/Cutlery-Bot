9f836987e1227f2bd9956f147291259a7e4efefd
Bspoones <ben.soones@gmail.com>
2021-12-18 16:45:03 +0000

    Cleaned up code

--- lib/modules/Utility/purge.py
+++ lib/modules/Utility/purge.py
@@ -1,26 +1,33 @@
-import hikari, tanjun
+"""
+/purge command
+Developed by Bspoones - Dec 2021
+Solely for use in the ERL discord bot
+Doccumentation: https://www.bspoones.com/ERL/Utility#Purge
+"""
+
+import asyncio, tanjun
 from lib.core.bot import Bot
 from lib.core.client import Client
 from tanjun.abc import Context as Context
-from . import COG_TYPE
-import asyncio
 
-purge_limit = 25
+MAX_PURGE = 25
 
 purge_component = tanjun.Component()
 
+# NOTE: Cooldowns still to be added
 @purge_component.add_slash_command
 @tanjun.with_int_slash_option("limit","Amount of messages to delete", default=1)
+@tanjun.as_slash_command("purge","Purges an amount of messages in the chat")
 async def purge_command(ctx: Context, limit: int):
-    if limit <= purge_limit:
-        msgs = (await ctx.rest.fetch_messages(ctx.channel_id).limit(limit))
+    if limit <= MAX_PURGE:
+        msgs = await ctx.rest.fetch_messages(ctx.channel_id).limit(limit)
         await ctx.rest.delete_messages(ctx.channel_id, msgs)
         await ctx.respond(f"Deleted {limit} messages.")
         await asyncio.sleep(2)
         await ctx.delete_initial_response()
         Bot.log_command(ctx,"purge",limit)
     else:
-        await ctx.respond(f"You tried to purge `{limit:,}` messages, which is more than the maximum ({purge_limit:,}).")
+        await ctx.respond(f"You tried to purge `{limit:,}` messages, which is more than the maximum ({MAX_PURGE:,}).")
         await asyncio.sleep(5)
         await ctx.delete_initial_response()
 
