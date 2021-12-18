8cc18c73216df07e3274f4a56a63ab29967d5702
Bspoones <ben.soones@gmail.com>
2021-12-18 16:45:51 +0000

    Cleaned up code

--- lib/modules/Utility/uptime.py
+++ lib/modules/Utility/uptime.py
@@ -1,10 +1,16 @@
-import hikari, tanjun
+"""
+/uptime command
+Developed by Bspoones - Dec 2021
+Solely for use in the ERL discord bot
+Doccumentation: https://www.bspoones.com/ERL/Utility#Uptime
+"""
+
+import tanjun, time
 from lib.core.bot import Bot
 from lib.core.client import Client
-import datetime as dt, time
 from tanjun.abc import Context as Context
 from humanfriendly import format_timespan
-from . import COG_TYPE
+from . import COG_TYPE, COG_LINK
 
 
 uptime_component = tanjun.Component()
@@ -12,19 +18,17 @@ uptime_component = tanjun.Component()
 @uptime_component.add_slash_command
 @tanjun.as_slash_command("uptime","Gets the current uptime of the bot")
 async def uptime_command(ctx: Context):
-    print(time.perf_counter())
     uptime = format_timespan((time.perf_counter()-ctx.client.metadata["start time"]))
     embed = Bot.auto_embed(
         type="info",
         author=f"{COG_TYPE}",
-        title=":clock1: Uptime",
+        author_url = COG_LINK,
+        title=":stopwatch: Uptime",
         description=f"> ERL uptime: `{uptime}`.",
         ctx=ctx
     )
     await ctx.respond(embed)
     Bot.log_command(ctx,"uptime")
-
-
 
 @tanjun.as_loader
 def load_components(client: Client):
