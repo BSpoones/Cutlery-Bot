11cfa8f7b4085f7b356a4326b490b98d3cc2c653
Bspoones <ben.soones@gmail.com>
2022-01-09 02:19:13 +0000

    Updated docstring

--- lib/modules/Utility/__init__.py
+++ lib/modules/Utility/__init__.py
@@ -1,2 +1,9 @@
+"""
+Utility module
+Developed by Bspoones - Dec 2021
+Solely for use in the ERL discord bot
+Doccumentation: https://www.bspoones.com/ERL/Utility
+"""
+
 COG_TYPE = "Utility"
 COG_LINK = "http://www.bspoones.com/ERL/Utility"
