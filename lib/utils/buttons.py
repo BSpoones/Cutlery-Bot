70c634984e9477d648d07de0ec9fdf2293558f84
Bspoones <ben.soones@gmail.com>
2022-01-23 18:32:00 +0000

    Added universal buttons

---
+++ lib/utils/buttons.py
@@ -0,0 +1,47 @@
+from tkinter import Button
+import hikari
+from hikari.messages import ButtonStyle
+
+
+
+DELETE_CUSTOM_ID = "AUTHOR_DELETE_BUTTON"
+
+EMPTY_ROW = (
+    hikari.impl.ActionRowBuilder()
+    .add_button(ButtonStyle.SECONDARY,"Expired")
+    .set_label("Buttons expired, run command again to view")
+    .set_is_disabled(True)
+    .add_to_container()
+)
+
+
+DELETE_ROW = (
+    hikari.impl.ActionRowBuilder()
+    .add_button(hikari.ButtonStyle.DANGER, DELETE_CUSTOM_ID)
+    .set_emoji("❌")
+    .add_to_container()
+)
+
+PAGENATE_ROW = (
+    hikari.impl.ActionRowBuilder()
+    .add_button(ButtonStyle.PRIMARY, "FIRST")
+    .set_emoji("⏮")
+    .add_to_container()
+    
+    .add_button(ButtonStyle.PRIMARY, "BACK")
+    .set_emoji("◀")
+    .add_to_container()
+
+    .add_button(ButtonStyle.PRIMARY, "NEXT")
+    .set_emoji("▶")
+    .add_to_container()
+    
+    .add_button(ButtonStyle.PRIMARY, "LAST")
+    .set_emoji("⏭")
+    .add_to_container()
+    
+    .add_button(ButtonStyle.DANGER, DELETE_CUSTOM_ID)
+    .set_emoji("❌")
+    .add_to_container()
+
+)
