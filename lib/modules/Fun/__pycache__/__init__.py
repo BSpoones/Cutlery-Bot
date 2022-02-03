b324f6ca5faa976b59a13a6b0c05fcdc03dd987f
Bspoones <ben.soones@gmail.com>
2022-02-03 22:49:24 +0000

    init

---
+++ lib/modules/Fun/__pycache__/__init__.py
@@ -0,0 +1,9 @@
+"""
+Fun module
+Developed by Bspoones - Feb 2022
+Solely for use in the ERL discord bot
+Doccumentation: https://www.bspoones.com/ERL/Fun
+"""
+
+COG_TYPE = "Fun"
+COG_LINK = "http://www.bspoones.com/ERL/Fun"
