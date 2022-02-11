be6b0391aa2460c0dc46c02d1921c670721c002e
Bspoones <ben.soones@gmail.com>
2022-02-11 15:45:05 +0000

    init

---
+++ lib/modules/Timetable/__init__.py
@@ -0,0 +1,9 @@
+"""
+Timetable module
+Developed by Bspoones - Feb 2022
+Solely for use in the Cutlery Bot discord bot
+Doccumentation: https://www.bspoones.com/Cutlery-Bot/Timetable
+"""
+
+COG_TYPE = "Timetable"
+COG_LINK = "http://www.bspoones.com/Cutlery-Bot/Timetable"
