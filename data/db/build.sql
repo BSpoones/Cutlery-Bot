0dfd0a93a516862b68f9d9da8b5dd3bb3fdf0381
Bspoones <ben.soones@gmail.com>
2022-02-12 21:38:51 +0000

    Updated data structure for the timetable module

--- data/db/build.sql
+++ data/db/build.sql
@@ -1,44 +1,61 @@
+/*
+Database configuration for Cutlery Bot
+Developed by BSpoones  Nov 21 - Feb 22
+Solely for use in the Cutlery Bot discord bot
+*/
+
 CREATE TABLE IF NOT EXISTS Lessons (
     LessonID BIGINT(24) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     GroupID INT(32),
     TeacherID INT(32),
+    SubjectID INT(32),
     DayOfWeek tinyint(6),
-    StartTime TIME,
-    EndTime TIME,
+    WeekNumber tinyint(2), -- 0 - 52, used to check multi week timetables
+    StartTime VARCHAR(4),
+    EndTime VARCHAR(4),
     Room VARCHAR(50)
-);
-
-CREATE TABLE IF NOT EXISTS Teachers (
-    TeacherID BIGINT(24) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    GroupID INT(32),
-    TeacherName VARCHAR(50),
-    TeacherSubject VARCHAR(100),
-    TeacherColour VARCHAR(7),
-    TeacherLink VARCHAR(256),
-    LessonOnline BOOL
 );
 
 CREATE TABLE IF NOT EXISTS LessonGroups (
     GroupID BIGINT(24) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     GroupOwnerID VARCHAR(18),
-    GuildID VARCHAR(18),
-    GroupCode VARCHAR(50),
     GroupName VARCHAR(50),
+    GroupCode VARCHAR(50),
+    GuildID VARCHAR(18),
     RoleID VARCHAR(18),
+    PingRoleID VARCHAR(18),
     RoleColour VARCHAR(7),
     CaregoryID VARCHAR(18),
-    LessonAnnouncementID VARCHAR(18),
+    AnnouncementID VARCHAR(18),
     NLDayID VARCHAR(18),
     NLTimeID VARCHAR(18),
     ImageLink VARCHAR(256),
     AlertTimes VARCHAR(100)
 );
 
+CREATE TABLE IF NOT EXISTS Teachers (
+    TeacherID BIGINT(24) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    GroupID BIGINT(32),
+    TeacherName VARCHAR(50),
+    TeacherColour VARCHAR(7),
+    TeacherLink VARCHAR(256),
+    LessonOnline BOOL
+);
+
+CREATE TABLE IF NOT EXISTS Subjects (
+    SubjectID BIGINT(24) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    TeacherID BIGINT(24),
+    SubjectName VARCHAR(100),
+    SubjectColour VARCHAR(7)
+);
+
 CREATE TABLE IF NOT EXISTS Students (
     StudentID BIGINT(24) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    GroupID int,
+    GroupID BIGINT(24),
     UserID VARCHAR(18),
-    Fullname text
+    Fullname VARCHAR (100),
+    Pings BOOL,
+    Moderator BOOL
 );
 
 CREATE TABLE IF NOT EXISTS Assignments(
@@ -55,7 +72,7 @@ CREATE TABLE IF NOT EXISTS Reminders (
     ReminderID BIGINT(24) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     CreatorID VARCHAR(18),
     TargetID VARCHAR(18),
-    GroupID VARCHAR(18),
+    GuildID VARCHAR(18),
     ChannelID VARCHAR(18),
     ReminderType VARCHAR(1), -- Either R or S (Repeating or single), 1 chars
     DateType VARCHAR(8), -- YYYYMMDD is the longest input with 8 chars
@@ -74,15 +91,5 @@ CREATE TABLE IF NOT EXISTS CommandLogs(
     ChannelID VARCHAR(18),
     Command text,
     Args text DEFAULT NULL,
-    TimeSent timestamp DEFAULT CURRENT_TIMESTAMP
-);
-
-CREATE TABLE IF NOT EXISTS MessageLogs(
-    MessageLogID BIGINT(24) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    UserID VARCHAR(18),
-    GuildID VARCHAR(18),
-    ChannelID VARCHAR(18),
-    MessageID VARCHAR(18),
-    Command text,
     TimeSent timestamp DEFAULT CURRENT_TIMESTAMP
 )
