7da3f139aa05d48fce45b841a4f825d7ee7d2dcd
Bspoones <ben.soones@gmail.com>
2022-02-12 21:39:15 +0000

    Created an instance of the Timetable class

--- lib/modules/Timetable/__init__.py
+++ lib/modules/Timetable/__init__.py
@@ -7,3 +7,14 @@ Doccumentation: https://www.bspoones.com/Cutlery-Bot/Timetable
 
 COG_TYPE = "Timetable"
 COG_LINK = "http://www.bspoones.com/Cutlery-Bot/Timetable"
+DAYS_OF_WEEK = ["monday","tuesday","wednesday","thursday","friday","saturday","sunday"]
+
+__version__ = "0"
+
+
+from lib.modules.Timetable.timetable_funcs import Timetable
+
+CB_TIMETABLE = Timetable()
+"""
+Instance of Timetable in timetable_funcs.py.
+"""
