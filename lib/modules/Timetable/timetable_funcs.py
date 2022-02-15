a4f74c82e0f7add3cf6a2841b5baa9a37c90f9ea
Bspoones <ben.soones@gmail.com>
2022-02-15 16:18:52 +0000

    Added `load_timetable`, `send_lesson_embed`, `send_lesson_cooldown`, `update_time_channels`, `get_group_ids_from_user` and misc functions

--- lib/modules/Timetable/timetable_funcs.py
+++ lib/modules/Timetable/timetable_funcs.py
@@ -4,20 +4,103 @@ Developed by Bspoones - Feb 2022
 Solely for use in the Cutlery Bot discord bot
 Doccumentation: https://www.bspoones.com/Cutlery-Bot/Timetable
 """
+import asyncio
+from random import random
+import re
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from apscheduler.triggers.cron import CronTrigger
-from datetime import date, datetime
 from PIL import Image
+import hikari
+from humanfriendly import format_timespan
+from tanjun import Client
+import tanjun, datetime
+
+import validators
+from lib.core.bot import bot, Bot
+from . import COG_LINK, COG_TYPE, DAYS_OF_WEEK
+from lib.utils import utilities as BotUtils
+
+from ...db import db
 # All IDs (GroupID, UserID etc) to be standardised in CamelCase
+
+"""
+DATA STRUCTURE:
+
+Lesson table:
+
+0 LessonID
+1 GroupID
+2 TeacherID
+3 SubjectID
+4 DayOfWeek
+5 WeekNumber
+6 StartTime
+7 EndTime
+8 Room
+
+Groups table
+
+0 GroupID
+1 GroupOwnerID
+2 GroupName
+3 GroupCode
+4 GuildID
+5 RoleID
+6 PingRoleID
+7 RoleColour
+8 CategoryID
+9 AnnouncementID
+10 NLDayID
+11 NLTimeID
+12 ImageLink
+13 AlertTimes
+
+Teachers table
+
+0 TeacherID
+1 GroupID
+2 TeacherName
+3 TeacherColour
+4 TeacherLink
+5 LessonOnline
+
+Subjects table
+
+0 SubjectID
+1 TeacherID
+2 SubjectName
+3 SubjectColour
+
+Students table
+
+0 StudentID
+1 GroupID
+2 UserID
+3 Fullname
+4 Pings
+5 Moderator
+
+Holidays table
+
+0 HolidayID
+1 GroupID
+2 StartDate
+3 EndDate
+
+"""
+
+HM_FMT = "%H%M"
+ONLINE_THUMBNAIL = "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9b/Google_Meet_icon_%282020%29.svg/934px-Google_Meet_icon_%282020%29.svg.png"
 
 class Timetable():
     def __init__(self):
         self.lesson_scheduler = AsyncIOScheduler
         self.load_timetable()
+        self.bot: hikari.GatewayBot = bot
     
     def load_timetable(self):
         """
-        Iterates through all entries on the Lessons table iin the database, adding
+        Iterates through all entries on the Lessons table in the database, adding
         them as jobs to the lesson scheduler.
         
         It calculates the neccesary countdown warning times before each lesson, adding
@@ -25,8 +108,78 @@ class Timetable():
         
         It then starts the scheduler
         """
-        pass
-    def send_lesson_embed(self,*args):
+        try: # Prevents multiple schedulers running at the same time
+            if (self.lesson_scheduler.state) == 1:
+                self.lesson_scheduler.shutdown(wait=False)
+            self.lesson_scheduler = AsyncIOScheduler()
+        except:
+            self.lesson_scheduler = AsyncIOScheduler()
+        
+        lessons = db.records("SELECT * FROM Lessons ORDER BY LessonID")
+        
+        for lesson in lessons:
+            GroupID = lesson[1]
+            DayOfWeek = lesson[4]
+            WeekNumber = lesson[5]
+            StartTime = lesson[6]
+            
+            GroupInfo: list[str] = db.record("SELECT * FROM LessonGroups WHERE GroupID = ?",GroupID)
+            WarningTimes = GroupInfo[13].split()
+            IntWarningTimes = list(map(int,WarningTimes))
+            """
+            Below changes the warning times so they only get sent between lessons.
+            E.G If a second lesson comes 15 mins after the first, there's no point
+            giving a 30 min warning for the 2nd lesson as it will be during the first
+            lesson
+            """
+            # The following sets a default date to 1900-01-01 + DayOfWeek + Time. This date will be the same as the required
+            # day of week and is used to calculate overlapping lessons and nothing else, it will IGNORE holidays
+            StartTimeDateTime = datetime.datetime.strptime(StartTime,HM_FMT) + datetime.timedelta(days=DayOfWeek + (7*WeekNumber))
+            # The 7*Weeknumber is used for multi week lessons if i ever do them
+            
+            # Will calculate the next occourance of the lesson in datetime format
+            ActualWarningTimes: list[int] = []
+            for WarningTime in IntWarningTimes:
+                TDelta = datetime.timedelta(minutes=WarningTime)
+                ElementDateTime = StartTimeDateTime -  TDelta
+                if not self.is_datetime_in_lesson((GroupID,),ElementDateTime):
+                    ActualWarningTimes.append(WarningTime)
+            if 0 not in ActualWarningTimes:
+                ActualWarningTimes.append(0) # Used for the "Your lesson is now" message
+                
+            # Scheduling the lesson with appropriate warning times
+            for WarningTime in ActualWarningTimes:
+                UpdatedDateTime = StartTimeDateTime - datetime.timedelta(minutes=WarningTime)
+                UpdatedDayOfWeek = UpdatedDateTime.weekday()
+                UpdatedHour = UpdatedDateTime.hour
+                UpdatedMinute = UpdatedDateTime.minute
+                
+                Trigger = CronTrigger(
+                    day_of_week= UpdatedDayOfWeek,
+                    hour= UpdatedHour,
+                    minute = UpdatedMinute,
+                    second = 0
+                )
+                if WarningTime == ActualWarningTimes[0]: # The earliest warning time, when the lesson embed appears
+                    self.lesson_scheduler.add_job(
+                        self.send_lesson_embed,
+                        Trigger,
+                        args = [lesson]
+                    )
+                    self.lesson_scheduler.add_job(
+                        self.send_lesson_countdown,
+                        Trigger,
+                        args = [lesson, WarningTime, ActualWarningTimes]
+                    )
+                else:
+                    self.lesson_scheduler.add_job(
+                        self.send_lesson_countdown,
+                        Trigger,
+                        args = [lesson, WarningTime, ActualWarningTimes]
+                    )
+        self.lesson_scheduler.start()
+    
+    async def send_lesson_embed(self,*args):
         """
         Sends lesson information as en embed at the earliest warning time before a
         lesson is due to start. It calculates lesson duration and other information,
@@ -36,8 +189,86 @@ class Timetable():
         `30 mins before >= lesson >= 30 mins after` (as long as that time isn't during another
         lesson)
         """
-        pass
-    def send_lesson_countdown(self,*args):
+        print(*args)
+        lesson = args[0]
+        GroupID = lesson[1]
+        # Can assume that the current datetime is the time the lesson is sent
+        LessonDatetime = datetime.datetime.today()
+        # Checks if lesson should send
+        if self.is_lesson_in_holiday(GroupID,LessonDatetime):
+            return # No lessons during a holiday so no need to carry on with this function
+        
+        # Assigning variables
+        
+        GroupID = lesson[1]
+        TeacherID = lesson[2]
+        SubjectID = lesson[3]
+        StartTime = lesson[6]
+        EndTime = lesson[7]
+        Room = lesson[8]
+        
+        GroupInfo: list[str] = db.record("SELECT * FROM LessonGroups WHERE GroupID = ?",GroupID)
+        
+        OutPutChannel = int(GroupInfo[9])
+        SchoolName = GroupInfo[2]
+        SchoolIconLink = GroupInfo[12]
+        
+        TeacherInfo: list[str] = db.record("SELECT * FROM Teachers WHERE TeacherID = ?",TeacherID)
+        
+        LessonTeacher = TeacherInfo[2]
+        LessonOnline = TeacherInfo[5]
+        if LessonOnline:
+            LessonLink = TeacherInfo[4]
+        
+        StartDateTime = datetime.datetime.strptime(StartTime,HM_FMT)
+        EndDateTime = datetime.datetime.strptime(EndTime,HM_FMT)
+        LessonDration = format_timespan((EndDateTime-StartDateTime).total_seconds())
+        
+        StartTime = StartDateTime.time()
+        EndTime = EndDateTime.time()
+        
+        LessonDatetime = BotUtils.next_occourance_of_time(StartTime)
+        LessonTimestamp = BotUtils.get_timestamp(LessonDatetime)
+        
+        if SubjectID is not None:
+            SubjectInfo: list[str] = db.record("SELECT * FROM Subjects WHERE SubjectID = ?", SubjectID)
+            LessonSubject = SubjectInfo[2]
+            title = f"{LessonSubject} with {LessonTeacher}"
+        else:
+            title = f"Lesson with {LessonTeacher}"
+            
+
+        description = f"**In {Room}**\n> Start: `{StartTime.strftime('%H:%M')}`\n> End: `{EndTime.strftime('%H:%M')}`\n> Duration: `{LessonDration}`"
+        
+        fields = [
+            ("Lesson start",f":clock1: <t:{LessonTimestamp}:R>",False)
+        ]
+        # Assignments to be added here as fields
+        
+        # Creating embed
+        if SubjectID is None:
+            EmbedColourHex = TeacherInfo[3]
+        else:
+            EmbedColourHex = SubjectInfo[3]
+        EmbedColour = hikari.Colour(int(f"0x{EmbedColourHex}",16))
+        
+        embed = Bot.auto_embed(
+            type="lesson",
+            author=COG_TYPE,
+            author_url = COG_LINK,
+            title = title,
+            url = LessonLink if LessonOnline else None,
+            description = description,
+            fields = fields,
+            schoolname = SchoolName,
+            iconurl = SchoolIconLink if SchoolIconLink is not None else None,
+            colour = EmbedColour,
+            thumbnail = SchoolIconLink if SchoolIconLink is not None else None,
+        )
+        
+        await self.bot.rest.create_message(channel=OutPutChannel,embed=embed)
+   
+    async def send_lesson_countdown(self,*args):
         """
         Sends lesson countdowns at the appropriate warning times, only pinging users who
         have opted in for pings. (Figure out a way for only opt in people to see this)
@@ -46,8 +277,49 @@ class Timetable():
         ------
         `@Role your lesson is in x minutes!`
         """
-        pass
-    def update_time_channels(self,GroupID: int):
+        lesson = args[0]
+        WarningTime = args[1]
+        ActualWarningTimes: list[int] = args[2]
+        
+        GroupID = lesson[1]       
+        # Can assume that the current datetime is the time the lesson is sent
+        WarningDatetime = datetime.datetime.today()
+        if self.is_lesson_in_holiday(GroupID,WarningDatetime):
+            return # No lessons during a holiday so no need to carry on with this function
+        if WarningTime == ActualWarningTimes[0]:
+            await asyncio.sleep(2) # Prevents countdown from appearing before embed
+        
+        GroupInfo: list[str] = db.record("SELECT * FROM LessonGroups WHERE GroupID = ?",GroupID)
+        
+        OutputPingRoleID = GroupInfo[6]
+        OutputChannelID = int(GroupInfo[9])
+        
+        if WarningTime == 0:
+            await self.bot.rest.create_message(OutputChannelID,f"<@&{OutputPingRoleID}> your lesson is now!",role_mentions=True)
+            await self.update_time_channels(GroupID)
+        else:
+            WarningTimeIndex = ActualWarningTimes.index(WarningTime)
+            # Shouldn't create an error when finding +1 th in a list as the last element is always 0 and is handled by the above statement.
+            DeleteAfter = (ActualWarningTimes[WarningTimeIndex] - ActualWarningTimes[WarningTimeIndex+1])*60
+            message = await self.bot.rest.create_message(OutputChannelID,f"<@&{OutputPingRoleID}> your lesson is in `{format_timespan(WarningTime*60)}`",role_mentions=True)
+            await asyncio.sleep(DeleteAfter)
+            await message.delete()
+        
+    def is_lesson_in_holiday(self,GroupID,DateTimeInput) -> bool:
+        """
+        Takes the datetime of a target lesson embed or countdown and checks if it takes place
+        during a group's set holiday.
+        """
+        Holidays = db.records("SELECT * FROM Holidays WHERE GroupID = ?",str(GroupID))
+        for holiday in Holidays:
+            StartDateTime: datetime = holiday[2]
+            EndDateTime: datetime = holiday[3]
+            if StartDateTime<=DateTimeInput<=EndDateTime: # If current time is inbetween these dates
+                return True
+        else: # If there are no holidays or it's not in a holiday
+            return False
+    
+    async def update_time_channels(self,GroupID: int):
         """
         Updates the voice channel names to show the correct day and times of the next lesson.
         Uses the `get_next_lesson` function for a single group and recovers the information
@@ -57,11 +329,27 @@ class Timetable():
         ----
             GroupID (int): GroupID from Groups Table in database
         """
-        pass
+        NextLesson = self.get_next_lesson((GroupID,))
+        GroupInfo = db.records("SELECT * FROM LessonGroups WHERE GroupID = ?",GroupID)
+
+        DayOfWeek = int(NextLesson[4])      
+        StartTime = NextLesson[6]
+        EndTime = NextLesson[7]
+        StartDateTime = datetime.datetime.strptime(StartTime,HM_FMT).time()
+        EndDateTime = datetime.datetime.strptime(EndTime,HM_FMT).time()
+        NLDayID = int(GroupInfo[10])
+        NLTimeID = int(GroupInfo[11])
+        NLDayStr = f"Next Lesson: {DAYS_OF_WEEK[DayOfWeek].capitalize()}"
+        NLTimeStr = f"Time: {StartDateTime.strftime('%H:%M')} - {EndDateTime.strftime('%H:%M')}"
+        
+        await self.bot.rest.edit_channel(NLDayID,NLDayStr)
+        await self.bot.rest.edit_channel(NLTimeID,NLTimeStr)
+        
+        
     def is_datetime_in_lesson(self, GroupIDs: tuple, datetime_input: datetime) -> bool:
         """
         Converts a datetime object to a lesson date to be searched. A datetime method is chosen
-        instead of a day and time method to accomodate timetables with multiple weeks.
+        instead of a day and time method to accomodate timetables with holidays.
 
         Args
         ----
@@ -73,7 +361,7 @@ class Timetable():
             bool: Returns a confirmation of wether or not this datetime is in a lesson, it does
             NOT give information about the lesson
         """
-        pass
+        return False
     def get_lesson_during_datetime(self, GroupIDs: tuple, datetime_input: datetime) -> tuple or None:
         """
         Similar to `is_datetime_in_lesson`, it uses a datetime object and a tuple of GroupIDs to find out
@@ -102,43 +390,79 @@ class Timetable():
             list or None: [description]
         """
         pass
-    def get_group_info_from_user(self,UserID) -> tuple or None:
+    
+    def get_group_ids_from_user(self,UserID) -> tuple or None:
         """
         Searches through the Students table on database for a discord UserID
         Will return a tuple of GroupIDs if found and None is the user isn't in
         any groups.
         """
-        pass
+        GroupIDs = db.records("SELECT GroupID FROM Students WHERE UserID = ?",UserID)
+        if GroupIDs == []:
+            return None
+        else:
+            return GroupIDs
+    
     def get_group_info_from_input(self,Group_Input) -> tuple or None:
         """
         Searches through the Groups table for an input.
-        This input could be a GroupCode or a GroupName but NOT an ID
+        This input could be a GroupCode, GroupName or a GroupID
         Similar to above, it will return the group info of the selected group
         """
         pass
-    def get_next_lesson(self,GroupID: int) -> tuple or None:
+    def get_next_lesson(self,GroupIDs: tuple[int]) -> tuple or None:
         """
         Uses the current datetime to find the next occourance of a lesson in a
-        group. 
+        tuple of groups. 
         
         Returns a Lesson tuple from the database
+        """
+        pass
+    
+    def get_assignments_for_lesson(self, lesson: list[str]) -> list[str] or None:
+        """
+        Takes a lesson entry and searches for any and all assignments due 
+        30 mins before <= lesson <= 30 mins after as long as those time brackets
+        don't fall in another lesson
+        
+        Returns None if no assignments are found
+        
+        NOTE: TO BE MOVED TO ASSIGNMENT FUNCS 
+        """
+        pass
+    def get_assignments_for_datetime(self,datetime_input: datetime) -> list[str] or None:
+        """
+        Takes a datetime entry and searches for any and all assignments due
+        on that day.
+        
+        Returns None if no assignments are found
+        
+        NOTE: TO BE MOVED TO ASSIGNMENT FUNCS 
+    
         """
         pass
     def is_image_link_valid(self, link: str) -> bool:
         """
         Validates a given link
         """
-        pass
+        if link is not None:
+            return validators.url(link)
+    
     def validate_hex_code(self, hex_code) -> bool:
         """
         Uses regular expressions to validate a hex code
         """
-        pass
+        if hex is not None:
+            match = re.search(r'^(?:[0-9a-fA-F]{3}){1,2}$', hex)
+            return match
+    
     def random_hex_colour(self) -> str:
         """
         Generates a random hex colour
         """
-        pass
+        hex = "{:06x}".format(random.randint(0, 0xFFFFFF))
+        return hex
+    
     def generate_weeklyschedule_image(self, UserID, Group=None) -> Image:
         """
         Generates an image showing all the lessons, assignments and reminders for a user.
@@ -146,3 +470,15 @@ class Timetable():
         """
         pass
     
+    def convert_db_datetime(self,db_input) -> datetime:
+        """
+        Converts a database datetime string into a datetime object
+        """
+        pass
+
+@tanjun.as_loader
+def load_components(client: Client):
+    # Tanjun loader here as Client looks through every python
+    # file for this func and causes an error if not present
+    # NOTE: This function is of no use, please ignore it
+    pass
