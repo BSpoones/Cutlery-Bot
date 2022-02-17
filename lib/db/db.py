08119a1817f19e4d95fd90c70381dcd08bfb6f3c
Bspoones <ben.soones@gmail.com>
2022-02-17 22:40:03 +0000

    Fixed the `Cache` class to cache values as well as commands

--- lib/db/db.py
+++ lib/db/db.py
@@ -5,6 +5,8 @@ from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from apscheduler.triggers.cron import CronTrigger
 from mysql.connector.errors import OperationalError
 import mysql
+from mysql.connector.cursor import CursorBase
+from mysql.connector import MySQLConnection
 with open("./secret/dbCredentials.json") as f:
     dbCredentials = json.load(f)
     host = dbCredentials["host"]
@@ -14,14 +16,14 @@ with open("./secret/dbCredentials.json") as f:
 
 BUILD_PATH = "./data/db/build.sql"
 
-cxn = connect(
+cxn: MySQLConnection = connect(
     host=host,
     user=user,
     password=password,
     database=database,
 
 )
-cur = cxn.cursor(prepared=True)
+cur: CursorBase = cxn.cursor(prepared=True)
 def interact_with_server():
 	"""
 	Prevents database disconnecting from inactivity by sending request to
@@ -72,39 +74,49 @@ def with_commit(func):
 class Cache:
 	def __init__(self):
 		self.recent_requests = [] # Limit of 5 items
+		self.recent_values = []
 		self.recent_outputs = []
 		self.request_timestamps = []
-	def check_cache(self,command) -> list | None:
+	def check_cache(self,command, values) -> list | None:
 		if command in self.recent_requests:
 			index = self.recent_requests.index(command)
-			output = self.recent_outputs[index]
-			timestamp = self.request_timestamps[index]
-			if int(datetime.datetime.today().timestamp()) - int(timestamp) < 10: # 10 second cache
-				return output
+			if values == self.recent_values[index]:
+				output = self.recent_outputs[index]
+				timestamp = self.request_timestamps[index]
+				if int(datetime.datetime.today().timestamp()) - int(timestamp) < 10: # 10 second cache
+					return output
+				else:
+					self.remove_item_from_cache(index)
 			else:
 				# Expired entry in requests
-				self.recent_requests.pop(index)
-				self.recent_outputs.pop(index)
-				self.request_timestamps.pop(index)
+				self.remove_item_from_cache(index)
 				return None
 		else:
 			return None
-	def add_to_cache(self,command,output):
+	def add_to_cache(self,command,values,output):
 		# Assumes that this output is not already in the cache
 		if len(self.recent_requests) >= 5:
 			self.recent_requests.append(command)
 			self.recent_outputs.append(output)
+			self.recent_values.append(values)
 			self.request_timestamps.append(datetime.datetime.today().timestamp())
    
-			self.recent_requests.pop(0)
-			self.recent_outputs.pop(0)
-			self.request_timestamps.pop(0)
+			self.remove_item_from_cache(0)
 		else:
 			self.recent_requests.append(command)
 			self.recent_outputs.append(output)
+			self.recent_values.append(values)
 			self.request_timestamps.append(datetime.datetime.today().timestamp())
+
+
+	def remove_item_from_cache(self,index):
+		self.recent_requests.pop(index)
+		self.recent_values.pop(index)
+		self.recent_outputs.pop(index)
+		self.request_timestamps.pop(index)
+
 	def show_cache(self):
-		print(self.recent_requests,self.recent_outputs,self.request_timestamps)
+		print(self.recent_requests,self.recent_values,self.recent_outputs,self.request_timestamps)
 
 db_cache = Cache()
 
@@ -133,24 +145,24 @@ def field(command, *values):
 
 
 def record(command, *values):
-	cache_output = db_cache.check_cache(command)
+	cache_output = db_cache.check_cache(command,values=(values))
 	if cache_output is not None:
 		return cache_output
 	else:
 		cur.execute(command, tuple(values))
 		output = cur.fetchone()
-		db_cache.add_to_cache(command,output)
+		db_cache.add_to_cache(command,values,output)
 		return output
 
 
 def records(command, *values):
-	cache_output = db_cache.check_cache(command)
+	cache_output = db_cache.check_cache(command,values=(values))
 	if cache_output is not None:
 		return cache_output
 	else:
 		cur.execute(command, tuple(values))
 		output = cur.fetchall()
-		db_cache.add_to_cache(command,output)
+		db_cache.add_to_cache(command,values,output)
 		return output
 
 
