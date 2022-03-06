d4b8361457af72bdb057e101fbf0081be19b10bc
Bspoones <ben.soones@gmail.com>
2022-03-06 21:52:15 +0000

    Fixed issue when typing messages privately

--- lib/modules/Utility/type.py
+++ lib/modules/Utility/type.py
@@ -21,32 +21,38 @@ type_component = tanjun.Component()
 @tanjun.as_slash_command("type","Gets Cutlery Bot to send a message")
 async def type_command(ctx: SlashContext, message: str, private: bool, channel: hikari.InteractionChannel = None):
     member = ctx.member
-    guild = ctx.get_guild()    
-    perms = tanjun.utilities.calculate_permissions(
-        member=member,
-        guild=guild,
-        roles={r.id: r for r in member.get_roles()},
-        channel = guild.get_channel(channel.id)
-    )
-    permissions = (str(perms).split("|"))
-    if channel is not None:
-        if str(channel.type) != "GUILD_TEXT":
+    guild = ctx.get_guild()
+    if channel is not None: # Calculating permissions for users
+        perms = tanjun.utilities.calculate_permissions(
+            member=member,
+            guild=guild,
+            roles={r.id: r for r in member.get_roles()},
+            channel = guild.get_channel(channel.id)
+        )
+        permissions = (str(perms).split("|"))
+        
+        if str(channel.type) != "GUILD_TEXT": # Channels can be voice, category or text
             raise ValueError("You can only select a text channel to send a message in.")
-        if "SEND_MESSAGES" not in permissions:
+        if "SEND_MESSAGES" not in permissions: # If a user can't send a message in a channel, then the bot shouldn't on their behalf
             raise PermissionError("You do not have permissions to send messages in this channel")
+        
     if private:
         if channel is not None:
+            # Creates a message in the channel, and then replies to the slash command
             await ctx.rest.create_message(channel.id,message,role_mentions=True,user_mentions=True)
             await ctx.create_initial_response(f"Message sent to <#{channel.id}>",flags= hikari.MessageFlag.EPHEMERAL)
         else:
+            # Replies to the slash command then sends a messsage without a reply, used to make sure nobody knows that a user sent that message
             await ctx.respond("Typing....")
             await ctx.delete_initial_response() # NOTE: To be improved, no idea how to though
             await ctx.get_channel().send(message,role_mentions=True,user_mentions=True)
     else:
         if channel is not None:
+            # Creates a message in the channel, and then replies to the slash command
             await ctx.rest.create_message(channel.id,message,role_mentions=True,user_mentions=True)
             await ctx.create_initial_response(f"Message sent to <#{channel.id}>",flags= hikari.MessageFlag.EPHEMERAL)
         else:
+            # Normal slash command reply with text
             await ctx.respond(message,role_mentions=True,user_mentions=True)
     Bot.log_command(ctx,"type",str(message),str(private))
 @tanjun.as_loader
