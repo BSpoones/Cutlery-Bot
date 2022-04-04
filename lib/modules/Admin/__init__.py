14716958c078143e07c269393c840308b4621777
Bspoones <ben.soones@gmail.com>
2022-04-04 18:51:46 +0100

    Init

---
+++ lib/modules/Admin/__init__.py
@@ -0,0 +1,9 @@
+"""
+Admin module
+Developed by Bspoones - Mar 2022
+Solely for use in the Cutlery Bot discord bot
+Doccumentation: https://www.bspoones.com/Cutlery-Bot/Admin
+"""
+
+COG_TYPE = "Admin"
+COG_LINK = "http://www.bspoones.com/Cutlery-Bot/Admin"
