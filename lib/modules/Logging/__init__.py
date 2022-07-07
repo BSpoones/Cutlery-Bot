09425c6351b2d3929566ee81f28da2d26b92d5ff
Bspoones <ben.soones@gmail.com>
2022-07-07 08:18:50 +0100

    Added `Logging`

---
+++ lib/modules/Logging/__init__.py
@@ -0,0 +1,11 @@
+"""
+Logging module
+Developed by Bspoones - May 2022
+Solely for use in the Cutlery Bot discord bot
+Doccumentation: https://www.bspoones.com/Cutlery-Bot/Logging
+"""
+
+__version__ = "0.0"
+
+COG_TYPE = "Logging"
+COG_LINK = "http://www.bspoones.com/Cutlery-Bot/Logging"
