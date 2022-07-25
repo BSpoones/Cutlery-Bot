a7b783277a176534ace3f4a143af493608a78318
Bspoones <ben.soones@gmail.com>
2022-07-25 22:40:49 +0100

    Added guild reaction event support

--- lib/core/event_handler.py
+++ lib/core/event_handler.py
@@ -1,4 +1,6 @@
-import hikari, tanjun, logging, time
+import hikari, tanjun, logging, time, datetime
+from humanfriendly import format_timespan
+from matplotlib.image import thumbnail
 from hikari.events.base_events import ExceptionEvent
 from data.bot.data import VERSION, OUTPUT_CHANNEL
 from lib.utils.utilities import update_bot_presence
@@ -15,7 +17,7 @@ class EventHandler():
             hikari.StoppedEvent: self.on_stopped,
             hikari.GuildJoinEvent : self.on_guild_join_event,
             hikari.GuildUnavailableEvent : self.on_guild_unavailable_event,
-            hikari.GuildAvailableEvent : self.on_guild_avalaible_event,
+            hikari.GuildAvailableEvent : self.on_guild_availaible_event,
             hikari.GuildLeaveEvent : self.on_guild_leave_event,
             hikari.GuildUpdateEvent : self.on_guild_update_event,
             hikari.BanCreateEvent : self.on_ban_create_event,
@@ -94,18 +96,19 @@ class EventHandler():
     # NOTE: The following events only affect the bot and therefore are set at a base level
     async def on_guild_join_event(self, event: hikari.GuildJoinEvent):
         await update_bot_presence(self.bot)
-        print(event.guild.created_at)
         embed = self.bot.auto_embed(
             type="Logging",
             author=COG_TYPE,
             author_url = COG_LINK,
             title = f"Joined {event.guild.name}",
             description = f"Members: {event.guild.member_count}\nOwner: <@{event.guild.owner_id}>",
+            thumbnail = event.guild.icon_url,
             colour = hikari.Colour(0x00FF00)
         )
         await self.bot.rest.create_message(OUTPUT_CHANNEL,embed=embed)
         
     async def on_guild_unavailable_event(self, event: hikari.GuildUnavailableEvent):
+        self.bot.client.metadata[f"UNAVAILABLE{event.guild_id}"] = datetime.datetime.now().timestamp()
         guild = await event.fetch_guild()
         embed = self.bot.auto_embed(
             type="Logging",
@@ -116,20 +119,30 @@ class EventHandler():
         )
         await self.bot.rest.create_message(OUTPUT_CHANNEL,embed=embed)
         
-    async def on_guild_avalaible_event(self, event: hikari.GuildAvailableEvent):
-        uptime = ((time.perf_counter()-self.bot.client.metadata["start time"]))
-        if uptime < 60:
-            # Prevents all guilds running this function when bot starts up
-            return
-        guild = await event.fetch_guild()
-        embed = self.bot.auto_embed(
-            type="Logging",
-            author=COG_TYPE,
-            author_url = COG_LINK,
-            title = f"`{guild.name}` is now available",
-            colour = hikari.Colour(0x023020)
-        )
-        await self.bot.rest.create_message(OUTPUT_CHANNEL,embed=embed)
+    async def on_guild_availaible_event(self, event: hikari.GuildAvailableEvent):
+        try:
+            is_unavailable: float = self.bot.client.metadata[f"UNAVAILABLE{event.guild_id}"]
+        except KeyError:
+            is_unavailable = False
+        if is_unavailable:
+            # Event will only send a message if a previously unavailable guild is now available
+            uptime = ((time.perf_counter()-self.bot.client.metadata["start time"]))
+            if uptime < 60:
+                # Prevents all guilds running this function when bot starts up
+                return
+            offline_time = (datetime.datetime.now().timestamp() - is_unavailable)
+            offline_time_formatted = format_timespan(offline_time)
+            guild = await event.fetch_guild()
+            embed = self.bot.auto_embed(
+                type="Logging",
+                author=COG_TYPE,
+                author_url = COG_LINK,
+                title = f"`{guild.name}` is now available",
+                description = f"Guild downtime: `{offline_time_formatted}`",
+                colour = hikari.Colour(0x023020)
+            )
+            self.bot.client.metadata.pop(f"UNAVAILABLE{event.guild_id}")
+            await self.bot.rest.create_message(OUTPUT_CHANNEL,embed=embed)
     
     async def on_guild_leave_event(self, event: hikari.GuildLeaveEvent):
         await update_bot_presence(self.bot)
@@ -157,6 +170,7 @@ class EventHandler():
     
     async def on_emojis_update_event(self, event: hikari.EmojisUpdateEvent):
         await logging_funcs.emoji_update(self.bot,event)
+    
     async def on_integration_create_event(self, event: hikari.IntegrationCreateEvent):
         pass
     async def on_integration_delete_event(self, event: hikari.IntegrationDeleteEvent):
@@ -169,33 +183,42 @@ class EventHandler():
     # Channel events
     async def on_guild_channel_create_event(self, event: hikari.GuildChannelCreateEvent):
         await logging_funcs.guild_channel_create(self.bot,event)
+    
     async def on_guild_channel_edit_event(self, event: hikari.GuildChannelUpdateEvent):
         await logging_funcs.guild_channel_edit(self.bot,event)
+    
     async def on_guild_channel_delete_event(self, event: hikari.GuildChannelDeleteEvent):
         await logging_funcs.guild_channel_delete(self.bot,event)
+    
     async def on_guild_pins_update_event(self, event: hikari.GuildPinsUpdateEvent):
         pass
     async def on_DM_pins_update_event(self, event: hikari.DMPinsUpdateEvent):
+        # Doesn't need logging
         pass
     async def on_invite_create_event(self, event: hikari.InviteCreateEvent):
         pass
     async def on_invite_delete_event(self, event: hikari.InviteDeleteEvent):
         pass
     async def on_webhook_update_event(self, event: hikari.WebhookUpdateEvent):
+        # Doesn't need logging
         pass
     
     # Member events
     async def on_member_create_event(self, event: hikari.MemberCreateEvent):
         await logging_funcs.on_member_create(self.bot,event)
+    
     async def on_member_update_event(self, event: hikari.MemberUpdateEvent):
         pass
+    
     async def on_member_delete_event(self, event: hikari.MemberDeleteEvent):
         await logging_funcs.on_member_delete(self.bot,event)
     
     # Message events
     async def on_guild_message_create_event(self, event: hikari.GuildMessageCreateEvent):
         await logging_funcs.message_create(self.bot,event)
+    
     async def on_guild_message_update_event(self, event: hikari.GuildMessageUpdateEvent):
+        return
         print(event.__class__.__name__)
         print(event.message.is_pinned)
         try:
@@ -206,13 +229,17 @@ class EventHandler():
         print(event.message_id)
         print(event.message.guild_id, event.message.channel_id, event.message_id)
         print(f"https://discord.com/channels/{event.message.guild_id}/{event.message.channel_id}/{ event.message_id}")
+    
     async def on_guild_message_delete_event(self, event: hikari.GuildMessageDeleteEvent):
         pass
     async def on_DM_message_create_event(self, event: hikari.DMMessageCreateEvent):
+        # Doesn't need logging
         pass
     async def on_DM_message_update_event(self, event: hikari.DMMessageUpdateEvent):
+        # Doesn't need logging
         pass
     async def on_DM_message_delete_event(self, event: hikari.DMMessageDeleteEvent):
+        # Doesn't need logging
         pass
     async def on_guild_bulk_message_delete_event(self, event: hikari.GuildBulkMessageDeleteEvent):
         pass
@@ -220,20 +247,28 @@ class EventHandler():
     # Reaction events
     async def on_guild_reaction_create_event(self, event: hikari.GuildReactionAddEvent):
         await logging_funcs.guild_reaction_add(self.bot,event)
+    
     async def on_guild_reaction_delete_event(self, event: hikari.GuildReactionDeleteEvent):
-        pass
+        await logging_funcs.guild_reaction_remove(self.bot,event)
+    
     async def on_DM_reaction_create_event(self, event: hikari.DMReactionAddEvent):
+        # Doesn't need logging
         pass
     async def on_DM_reaction_delete_event(self, event: hikari.DMReactionDeleteEvent):
+        # Doesn't need logging
         pass
+    
     async def on_guild_reaction_delete_all_event(self, event: hikari.GuildReactionDeleteAllEvent):
-        pass
+        await logging_funcs.guild_reaction_delete_all(self.bot,event)
+    
     async def on_DM_reaction_deleter_all_event(self, event: hikari.DMReactionDeleteAllEvent):
+        # Doesn't need logging
         pass
     
     # Role events
     async def on_role_create_event(self, event: hikari.RoleCreateEvent):
         await logging_funcs.role_create(self.bot, event)
+    
     async def on_role_update_event(self, event: hikari.RoleUpdateEvent):
         pass
     async def on_role_delete_event(self, event: hikari.RoleDeleteEvent):
@@ -253,16 +288,21 @@ class EventHandler():
     
     # Typing events
     async def on_guild_typing_event(self, event: hikari.GuildTypingEvent):
+        # Really doesn't need logging
         pass
     async def on_DM_typing_event(self, event: hikari.DMTypingEvent):
+        # Doesn't need logging
         pass
     
     # User events
     async def on_own_user_update_event(self, event: hikari.OwnUserUpdateEvent):
+        # Doesn't need logging
         pass
     
     # Voice events
     async def on_voice_state_update_event(self, event: hikari.VoiceStateUpdateEvent):
-        pass
+        await logging_funcs.on_voice_state_update(self.bot,event)
+    
     async def on_voice_server_update_event(self, event: hikari.VoiceServerUpdateEvent):
+        # Doesn't need logging
         pass
