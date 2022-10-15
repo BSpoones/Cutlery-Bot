8da20e4cd78822da6ea9eb49f7dbe61bbfe83f77
Bspoones <ben.soones@gmail.com>
2022-10-15 05:26:33 +0100

    Updated for new db

--- lib/modules/Logging/logging_commands.py
+++ lib/modules/Logging/logging_commands.py
@@ -8,19 +8,21 @@ Documentation: https://www.bspoones.com/Cutlery-Bot/Logging#LoggingCommands
 import tanjun, hikari
 from tanjun import Client
 from tanjun.abc import SlashContext
-from data.bot.data import EVENT_TYPES
-from lib.core.bot import Bot
-from lib.modules.Logging import COG_LINK, COG_TYPE
 from mysql.connector.errors import InterfaceError
-from ...db import db
+
+from data.bot.data import EVENT_TYPES
+from lib.core.error_handling import CustomError
+from lib.db import db
+from lib.modules.Logging import COG_LINK, COG_TYPE
+from lib.utils.command_utils import auto_embed, log_command
 
 logging_component = tanjun.Component()
 logging_group = logging_component.with_slash_command(tanjun.slash_command_group("logging","Logging module"))
 
-NL = "\n"
+NL = "\n" # f strings dislike these
 
+# Binds event types to permissions
 LOG_PRESETS = {
-    # Binds event types to permissions
     "None": [],
     "Join-Leave log": [
         "MemberCreateEvent",
@@ -29,6 +31,7 @@ LOG_PRESETS = {
     "All": EVENT_TYPES
 }
 
+# Following lists the contents of each event preset 
 EVENT_PRESETS = {
     "Bans": [
         "BanCreateEvent",
@@ -36,7 +39,6 @@ EVENT_PRESETS = {
         ],
     "Emoji changes": [
         "EmojisUpdateEvent",
-        
         ],
     "Message deletions": [
         "GuildMessageCreateEvent",
@@ -75,102 +77,79 @@ EVENT_PRESETS = {
 }
 
 @logging_group.with_command
+@tanjun.with_author_permission_check(hikari.Permissions.MANAGE_GUILD)
 @tanjun.with_str_slash_option("preset","Choose to log a preset of choices", choices=LOG_PRESETS.keys())
 @tanjun.with_channel_slash_option("channel","Select an output channel",default= None)
-@tanjun.as_slash_command("enable","Sets up a logging instance to a channel")
-async def logging_enable_command(ctx: SlashContext, preset, channel: hikari.InteractionChannel = None):
-    guild = ctx.get_guild()
-    member = ctx.member
+@tanjun.as_slash_command("setup","Sets up a logging instance to a channel")
+async def logging_setup_command(ctx: SlashContext, preset: str, channel: hikari.InteractionChannel = None):
+    # Gets current channel if none is given
     if channel is None:
         channel = await ctx.fetch_channel()
-
-    # Calculating permissions for users
-    perms = tanjun.utilities.calculate_permissions(
-        member=member,
-        guild=guild,
-        roles={r.id: r for r in member.get_roles()},
-        channel = guild.get_channel(channel.id)
-    )
-    permissions = (str(perms).split("|"))
     
     # Checking if channel is a text channel
-    if str(channel.type.name) != "GUILD_TEXT": # Channels can be voice, category or text
-        raise ValueError("You can only select a text channel to log messages to.")
-    
-    # Checking if user appropriate permissions to create a logging instance
-    if "MANAGE_GUILD" not in permissions: # If a user can't send a message in a channel, then the bot shouldn't on their behalf
-        raise PermissionError("You do not have permissions to setup a log channel.")
+    if str(channel.type.name) != "GUILD_TEXT":
+        raise CustomError(f"`{str(channel.type.name)}` type selected","You can only select a text channel to log messages to.")
     
     await ctx.defer() # Used for when preset = All since adding 53 items to a database takes longer than the 3s thinking limit
     
     # Presence check of the proposed channel for a logging instance
-    instance = db.records("SELECT * FROM LogChannel WHERE GuildID = ? AND ChannelID = ?",str(guild.id),str(channel.id))
+    instance = db.records("SELECT * FROM log_channel WHERE guild_id = ? AND channel_id = ?", str(ctx.guild_id), str(channel.id))
     if instance != []:
-        raise LookupError("This channel already has a logging instance.\nUse `/logging add` to add events to log.")
+        raise CustomError("This channel already has a logging instance","Use `/logging add` to add events to log or `/logging delete` to delete the current instance.")
     
     # Assuming the user has perms and there isn't a logging instance for said channel
-    db.execute("INSERT INTO LogChannel(GuildID,ChannelID) VALUES (?,?)", str(guild.id),str(channel.id))
+    db.execute("INSERT INTO log_channel(guild_id,channel_id) VALUES (?,?)", str(ctx.guild_id),str(channel.id))
+    
     # Adding preset permissions
     events = LOG_PRESETS[preset]
-    command = "INSERT INTO ChannelLogAction VALUES ((SELECT LogChannelID FROM LogChannel WHERE GuildID = ? AND ChannelID = ?), (SELECT ActionID from LogAction WHERE ActionName = ?));"
-    data = [(str(guild.id), str(channel.id),event) for event in events]
+    command = "INSERT INTO channel_log_action VALUES ((SELECT log_channel_id FROM log_channel WHERE guild_id = ? AND channel_id = ?), (SELECT action_id from log_action WHERE action_name = ?));"
+    data = [(str(ctx.guild_id), str(channel.id),event) for event in events]
     db.multiexec(command,data)
     db.commit()
     
-    embed = Bot.auto_embed(
+    embed = auto_embed(
         type="info",
         author=COG_TYPE,
         author_url = COG_LINK,
         title = ":white_check_mark: Logging instance created",
-        description = f"Logging instance bound to <#{channel.id}> containing the preset `{preset}`",
+        description = f"Logging instance bound to <#{channel.id}> using the preset `{preset}`",
         ctx=ctx
     )
-    
+    log_command(ctx, "logging setup")
     await ctx.edit_initial_response(embed=embed)
 
 @logging_group.with_command
+@tanjun.with_author_permission_check(hikari.Permissions.MANAGE_GUILD)
 @tanjun.with_str_slash_option("preset","Choose to log a preset of choices", choices=EVENT_PRESETS.keys())
 @tanjun.with_channel_slash_option("channel","Select the logging channel",default= None)
 @tanjun.as_slash_command("add","Adds an event group to log in a logging instance")
-async def logging_add_command(ctx: SlashContext, preset, channel: hikari.InteractionChannel = None):  
-    guild = ctx.get_guild()
-    member = ctx.member
+async def logging_add_command(ctx: SlashContext, preset: str, channel: hikari.InteractionChannel = None):  
+    # Gets the current channel if none is given
     if channel is None:
         channel = await ctx.fetch_channel()
-
-    # Calculating permissions for users
-    perms = tanjun.utilities.calculate_permissions(
-        member=member,
-        guild=guild,
-        roles={r.id: r for r in member.get_roles()},
-        channel = guild.get_channel(channel.id)
-    )
-    permissions = (str(perms).split("|"))
     
     # Checking if channel is a text channel
     if str(channel.type.name) != "GUILD_TEXT": # Channels can be voice, category or text
-        raise ValueError("You can only select a text channel to log messages to.")
-    
-    # Checking if user appropriate permissions to create a logging instance
-    if "MANAGE_GUILD" not in permissions: # If a user can't send a message in a channel, then the bot shouldn't on their behalf
-        raise PermissionError("You do not have permissions to edit a log channel.")
-    
-    await ctx.defer() # Used for when preset = All since adding 53 items to a database takes longer than the 3s thinking limit
+        raise CustomError(f"`{str(channel.type.name)}` type selected","You can only select a text channel to log messages to.")
+
+    await ctx.defer() # Running this command *should* take less than 3s, but just in case
     
     # Presence check of the proposed channel for a logging instance
-    instance = db.records("SELECT * FROM LogChannel WHERE GuildID = ? AND ChannelID = ?",str(guild.id),str(channel.id))
+    instance = db.records("SELECT * FROM log_channel WHERE guild_id = ? AND channel_id = ?",str(ctx.guild_id),str(channel.id))
     if instance == []:
-        raise LookupError("This channel does not have a logging instance setup, use `/logging setup` to set one up.")
+        raise CustomError("No logging instance found","This channel does not have a logging instance setup. Use `/logging setup` to set one up.")
     
     # Adding preset permissions
     events = EVENT_PRESETS[preset]
-    command = "INSERT INTO ChannelLogAction VALUES ((SELECT LogChannelID FROM LogChannel WHERE GuildID = ? AND ChannelID = ?), (SELECT ActionID from LogAction WHERE ActionName = ?));"
-    data = [(str(guild.id), str(channel.id),event) for event in events]
+    command = "INSERT INTO channel_log_action VALUES ((SELECT log_channelID FROM log_channel WHERE guild_id = ? AND channel_id = ?), (SELECT action_id from log_action WHERE action_name = ?));"
+    data = [(str(ctx.guild_id), str(channel.id),event) for event in events]
+    
+    # Adds all events to db, if an InterfaceError occurs, then it already exists in the database
     try:
         db.multiexec(command,data)
         db.commit()
     except InterfaceError:
-        embed = Bot.auto_embed(
+        embed = auto_embed(
             type="error",
             author=COG_TYPE,
             author_url = COG_LINK,
@@ -180,9 +159,8 @@ async def logging_add_command(ctx: SlashContext, preset, channel: hikari.Interac
         )
         
         await ctx.edit_initial_response(embed=embed)
-        EVENT_PRESETS[preset]
 
-    embed = Bot.auto_embed(
+    embed = auto_embed(
         type="info",
         author=COG_TYPE,
         author_url = COG_LINK,
@@ -190,91 +168,65 @@ async def logging_add_command(ctx: SlashContext, preset, channel: hikari.Interac
         description = f"Logging instance bound to <#{channel.id}> has the following events added:\n\n**{preset}**```\n{NL.join(EVENT_PRESETS[preset])}```",
         ctx=ctx
     )
+    log_command(ctx, "logging add")
     
     await ctx.edit_initial_response(embed=embed)
 
 @logging_group.with_command
+@tanjun.with_author_permission_check(hikari.Permissions.MANAGE_GUILD)
 @tanjun.with_channel_slash_option("channel","Select a channel to send the message in",default= None)
-@tanjun.as_slash_command("disable","Disables a logging instance")
-async def logging_disable_command(ctx: SlashContext, channel: hikari.InteractionChannel = None):
-    guild = ctx.get_guild()
-    member = ctx.member
+@tanjun.as_slash_command("delete","Deletes a logging instance")
+async def logging_delete_command(ctx: SlashContext, channel: hikari.InteractionChannel = None):
     if channel is None:
         channel = await ctx.fetch_channel()
-
-    # Calculating permissions for users
-    perms = tanjun.utilities.calculate_permissions(
-        member=member,
-        guild=guild,
-        roles={r.id: r for r in member.get_roles()},
-        channel = guild.get_channel(channel.id)
-    )
-    permissions = (str(perms).split("|"))
-
-    # Checking if user appropriate permissions to create a logging instance
-    if "MANAGE_GUILD" not in permissions: # If a user can't send a message in a channel, then the bot shouldn't on their behalf
-        raise PermissionError("You do not have permissions to remove a log channel.")
     
     await ctx.defer()
     
     # Presence check
-    log_channel = db.record("SELECT * FROM LogChannel WHERE ChannelID = ?",str(channel.id))
+    log_channel = db.record("SELECT * FROM log_channel WHERE channel_id = ?",str(channel.id))
     if log_channel is None:
-        raise LookupError("No logging instance found in this channel. Use `/logging setup` to create a logging instance")
+        raise CustomError("No logging instance found","No logging instance found in this channel. Use `/logging setup` to create a logging instance")
     
-    db.execute("DELETE FROM LogChannel WHERE ChannelID = ?",str(channel.id))
+    db.execute("DELETE FROM log_channel WHERE channel_id = ?",str(channel.id))
     db.commit()
     
-    embed = Bot.auto_embed(
+    embed = auto_embed(
         type="info",
         author=COG_TYPE,
         author_url = COG_LINK,
         title = ":x: Logging instance removed",
-        description = f"Logging instance bound to <#{channel.id}> has been disabled.\n",
+        description = f"Logging instance bound to <#{channel.id}> has been removed.\n",
         ctx=ctx
     )
+    log_command(ctx, "logging delete")
     
     await ctx.edit_initial_response(embed=embed)
 
 @logging_group.with_command
+@tanjun.with_author_permission_check(hikari.Permissions.MANAGE_GUILD)
 @tanjun.with_str_slash_option("preset","Choose to remove a preset of choices", choices=EVENT_PRESETS.keys())
 @tanjun.with_channel_slash_option("channel","Select a channel",default= None)
 @tanjun.as_slash_command("remove","Removes an event group to log in a logging instance")
 async def logging_remove_command(ctx: SlashContext, preset, channel: hikari.InteractionChannel = None):  
-    guild = ctx.get_guild()
-    member = ctx.member
     if channel is None:
         channel = await ctx.fetch_channel()
 
-    # Calculating permissions for users
-    perms = tanjun.utilities.calculate_permissions(
-        member=member,
-        guild=guild,
-        roles={r.id: r for r in member.get_roles()},
-        channel = guild.get_channel(channel.id)
-    )
-    permissions = (str(perms).split("|"))
-        
-    # Checking if user appropriate permissions to create a logging instance
-    if "MANAGE_GUILD" not in permissions: # If a user can't send a message in a channel, then the bot shouldn't on their behalf
-        raise PermissionError("You do not have permissions to edit a log channel.")
-    
-    await ctx.defer() # Used for when preset = All since adding 53 items to a database takes longer than the 3s thinking limit
+    await ctx.defer()
     
     # Presence check of the proposed channel for a logging instance
-    instance = db.records("SELECT * FROM LogChannel WHERE GuildID = ? AND ChannelID = ?",str(guild.id),str(channel.id))
+    instance = db.records("SELECT * FROM log_channel WHERE guild_id = ? AND channel_id = ?",str(ctx.guild_id),str(channel.id))
     if instance == []:
-        raise LookupError("This channel does not have a logging instance setup, use `/logging setup` to set one up.")
+        raise CustomError("No logging instance found","This channel does not have a logging instance setup, use `/logging setup` to set one up.")
     
     # Adding preset permissions
     events = EVENT_PRESETS[preset]
-    command = "DELETE FROM ChannelLogAction WHERE LogChannelID = (SELECT LogChannelID FROM LogChannel WHERE GuildID = ? AND ChannelID = ?) AND ActionID = (SELECT ActionID from LogAction WHERE ActionName = ?);"
-    data = [(str(guild.id), str(channel.id),event) for event in events]
+    command = "DELETE FROM channel_log_action WHERE log_channel_id = (SELECT log_channel_id FROM log_channel WHERE guild_id = ? AND channel_id = ?) AND action_id = (SELECT action_id from log_action WHERE action_name = ?);"
+    data = [(str(ctx.guild_id), str(channel.id),event) for event in events]
     try:
         db.multiexec(command,data)
         db.commit()
     except InterfaceError:
-        embed = Bot.auto_embed(
+        embed = auto_embed(
             type="error",
             author=COG_TYPE,
             author_url = COG_LINK,
@@ -285,7 +237,7 @@ async def logging_remove_command(ctx: SlashContext, preset, channel: hikari.Inte
         
         await ctx.edit_initial_response(embed=embed)
         
-    embed = Bot.auto_embed(
+    embed = auto_embed(
         type="info",
         author=COG_TYPE,
         author_url = COG_LINK,
@@ -294,6 +246,7 @@ async def logging_remove_command(ctx: SlashContext, preset, channel: hikari.Inte
         ctx=ctx
     )
     
+    log_command(ctx, "logging remove")
     await ctx.edit_initial_response(embed=embed)
 
 @logging_group.with_command
@@ -363,6 +316,7 @@ async def logging_unignore(ctx: SlashContext, channel: hikari.GuildChannel, log_
         ctx=ctx
     )
     await ctx.respond(embed=embed)
+
 
 @tanjun.as_loader
 def load_components(client: Client):
