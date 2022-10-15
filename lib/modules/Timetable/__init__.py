049cda004f33d3229c9896ee8a1891852444d96c
Bspoones <ben.soones@gmail.com>
2022-10-15 05:29:03 +0100

    Updated to v2.0

--- lib/modules/Timetable/__init__.py
+++ lib/modules/Timetable/__init__.py
@@ -9,12 +9,14 @@ COG_TYPE = "Timetable"
 COG_LINK = "http://www.bspoones.com/Cutlery-Bot/Timetable"
 DAYS_OF_WEEK = ["monday","tuesday","wednesday","thursday","friday","saturday","sunday"]
 
-__version__ = "0"
+__version__ = "2.0"
 
-
+import datetime
 from lib.modules.Timetable.timetable_funcs import Timetable
 
 CB_TIMETABLE = Timetable()
+
+# print(CB_TIMETABLE.get_next_lesson("1"))
 """
 Instance of Timetable in timetable_funcs.py.
 """
