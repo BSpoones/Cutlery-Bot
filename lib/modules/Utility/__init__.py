15e3bede1005624008123ceca5e42be3dd630cbd
Bspoones <ben.soones@gmail.com>
2022-10-23 21:53:36 +0100

    Added version

--- lib/modules/Utility/__init__.py
+++ lib/modules/Utility/__init__.py
@@ -5,5 +5,7 @@ Solely for use in the Cutlery Bot discord bot
 Documentation: https://www.bspoones.com/Cutlery-Bot/Utility
 """
 
+__version__ = "1.0"
+
 COG_TYPE = "Utility"
 COG_LINK = "http://www.bspoones.com/Cutlery-Bot/Utility"
