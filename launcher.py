705759d204c78f371a18be8fdca63a263f7d0fc4
Bspoones <ben.soones@gmail.com>
2022-10-23 21:51:52 +0100

    Updated to include basic docstring

--- launcher.py
+++ launcher.py
@@ -1,4 +1,15 @@
-from lib.core.bot import bot
+"""
+------- [ CUTLERY BOT ] ------- 
 
+Developed by BSpoones:
+6th November 2021 - Current
+Last Update: 2022-10-23 - v0.9.3
+
+More information: https://bspoones.com/Cutlery-Bot
+
+launcher.py
+- Use me to start Cutlery Bot!
+"""
+from lib.core.bot import bot
 
 bot.run()
